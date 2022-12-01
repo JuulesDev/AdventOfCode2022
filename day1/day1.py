b1eead896839631aad60ffe900ab2ab9ece96a7b
Jules <fluojustplay@gmail.com>
2022-12-01 20:43:24 +0100

    ✨ - Completed part two of day one.

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,14 @@
 
+def store_value(value: int, pos: int, tab: list[int]):
+    for i_store in range(pos):
+        tab[i_store] = tab[i_store + 1]
+    tab[pos] = value
+
+
 if __name__ == '__main__':
-    most_calories = 0
+    top_calories = [0, 0, 0]
     current_elf_calories = 0
+
     with open('./day1-input', 'r') as file:
         for line in file.readlines():
             content = line.strip()
@@ -9,8 +16,18 @@ if __name__ == '__main__':
             if content != '':
                 current_elf_calories += int(content)
             else:
-                if current_elf_calories > most_calories:
-                    most_calories = current_elf_calories
+                # End of elf calories
+
+                # If better than best
+                if current_elf_calories > top_calories[-1]:
+                    store_value(current_elf_calories, len(top_calories)-1, top_calories)
+
+                # Search for right place
+                elif current_elf_calories > top_calories[0]:
+                    for i_elf in range(len(top_calories)):
+                        if current_elf_calories <= top_calories[i_elf]:
+                            store_value(current_elf_calories, i_elf-1, top_calories)
+                            break
                 current_elf_calories = 0
 
-    print(most_calories)
+    print(sum(top_calories))
