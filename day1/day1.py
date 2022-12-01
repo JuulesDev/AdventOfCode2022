a8f12777c8fdf63666088e9396a026073a945430
Jules <fluojustplay@gmail.com>
2022-12-01 21:53:56 +0100

    🎨 - Improved day 1 code structure.

--- day1/day1.py
+++ day1/day1.py
@@ -5,9 +5,22 @@ def store_value(value: int, pos: int, tab: list[int]):
         tab[i_store] = tab[i_store + 1]
     tab[pos] = value
 
+def manage_elf(elf_calories: int, top_calories: list[int]):
+    """Compare elf calories to the top, and add it to the top if needed."""
+    if elf_calories > top_calories[-1]: 
+        # Edge case if current calories is better than the top
+        store_value(elf_calories, len(top_calories)-1, top_calories)
+    elif elf_calories > top_calories[0]:
+        # If elf fit in the top
+        for i_elf in range(len(top_calories)):
+            # Looking for the right place to put the current elf
+            if elf_calories <= top_calories[i_elf]:
+                store_value(elf_calories, i_elf-1, top_calories)
+                break
+
 
 if __name__ == '__main__':
-    top_calories = [0, 0, 0]
+    top_calories = [0 for _ in range(3)]
     current_calories = 0
 
     with open('./day1-input', 'r') as file:
@@ -17,15 +30,8 @@ if __name__ == '__main__':
             if content != '': # Add calories to current Elf
                 current_calories += int(content)
             else: # End of current Elf
-                if current_calories > top_calories[-1]: 
-                    # Edge case if current calories is better than the top
-                    store_value(current_calories, len(top_calories)-1, top_calories)
-                elif current_calories > top_calories[0]:
-                    for i_elf in range(len(top_calories)):
-                        # Looking for the right place to put the current elf
-                        if current_calories <= top_calories[i_elf]:
-                            store_value(current_calories, i_elf-1, top_calories)
-                            break
+                manage_elf(current_calories, top_calories)
                 current_calories = 0
 
     print(sum(top_calories))
+
