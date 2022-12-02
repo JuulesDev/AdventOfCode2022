b9552533266b01651ad70dcffc9ed65ed8a7c884
Jules <fluojustplay@gmail.com>
2022-12-02 11:11:52 +0100

    ✨ - Added day2, changed day1 input file name.

---
+++ day2/day2.py
@@ -0,0 +1,7 @@
+
+
+if __name__ == '__main__':
+    with open('day2-input.txt', 'r') as file:
+        for line in file.readlines():
+            content = line.strip().split()
+            print(content)
